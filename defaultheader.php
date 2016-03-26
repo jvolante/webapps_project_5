723414b3cfea235400698aedaaa1e2c606b850d2
Jackson Volante <hikerjv@gmail.com>
2016-03-26 19:40:04 -0400

    Initial commit

---
+++ defaultheader.php
@@ -0,0 +1,6 @@
+<?php
+  // A bunch of tags that we need in the header of all of our pages.
+  echo '<script src="/js/plugins.js"></script>';
+  echo '<link rel="stylesheet" href="/css/normalize.css" media="screen" title="no title" charset="utf-8">';
+  echo '<link rel="stylesheet" href="/css/main.css" media="screen" title="no title" charset="utf-8">';
+?>
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <?php include 'defaultheader.php'; ?>
+    <title>People's Choice Awards</title>
+  </head>
+  <body>
+    <div id="signinbutton">
+      Sign in button goes here
+    </div>
+    <section>
+      Title Area
+      <!-- TODO: Title image and whatever else -->
+    </section>
+    <section>
+      Projects Area
+      <?php
+        // TODO: Generate projects table from the database
+      ?>
+    </section>
+  </body>
+</html>
