d205e01b5df876f94ee9735f38fa77bd2ae66886
Kidron Filbrun <kfilbrun@gmail.com>
2016-04-02 16:29:34 -0400

    added get users php and tabs to index.php

--- defaultheader.php
+++ defaultheader.php
@@ -4,6 +4,7 @@
 ?>
 <!-- A bunch of tags that we need in the header of all of our pages. -->
 <script src="js/plugins.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <link href='https://fonts.googleapis.com/css?family=Oswald|Lato' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" href="css/normalize.css" media="screen" title="no title" charset="utf-8">
 <link rel="stylesheet" href="css/main.css" media="screen" title="no title" charset="utf-8">
