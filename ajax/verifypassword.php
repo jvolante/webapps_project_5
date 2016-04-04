51c89e7a705ea20e6eb922d1ff7b4b6c66a3d96b
Jackson Volante <hikerjv@gmail.com>
2016-04-04 17:07:25 -0400

    fixed password verification, fixed some styling

--- ajax/verifypassword.php
+++ ajax/verifypassword.php
@@ -1,6 +1,9 @@
 <?php
   session_start();
 
+  include '../sqlserverparams.php';
+  include '../params.php';
+
   if(isset($_POST["password"]) && isset($_POST["username"])){
     $putitivePassword = $_POST["password"];
     $username = $_POST["username"];
@@ -12,7 +15,7 @@
       die("Database Connection Failed");
     }
 
-    $result = $conn->query("SELECT password_hash FROM pca.jk_users WHERE linux_user = '$username';");
+    $result = $conn->query("SELECT password_hash, name FROM pca.jk_users WHERE linux_user = '$username';");
 
     if($result->num_rows == 0){
       die("Error, no entry was found for $username");
