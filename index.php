b6bd61d359c2b5666a33cc943e6252237e225158
Kidron Filbrun <kfilbrun@gmail.com>
2016-04-04 23:48:19 -0400

    Front page live update is working, link added to admin page

--- index.php
+++ index.php
@@ -8,6 +8,8 @@
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+	<script src="./js/randomColor/randomColor.js"></script>
+	<script src="./js/chartjs/Chart.js"></script>
     <script src="js/tether.min.js"></script>
     <script src="js/drop.min.js"></script>
     <script src="js/sha512.js"></script>
@@ -52,13 +54,15 @@
 		  isFirst = true
 
           $.each(data, function(key, value){
-			if(isFirst){
-				$("#crossbarLinkArea").append('<a href="http://judah.cedarville.edu/~' + key + '/cs4220.html" class="crossbar">' + value + '</a>');
-				isFirst = false;
-			}
-			else {
-				$("#crossbarLinkArea").append('\n&bull;\n');
-				$("#crossbarLinkArea").append('<a href="http://judah.cedarville.edu/~' + key + '/cs4220.html" class="crossbar">' + value + '</a>');
+			if(value != "admin"){
+				if(isFirst){
+					$("#crossbarLinkArea").append('<a href="http://judah.cedarville.edu/~' + key + '/cs4220.html" class="crossbar">' + value + '</a>');
+					isFirst = false;
+				}
+				else {
+					$("#crossbarLinkArea").append('\n&bull;\n');
+					$("#crossbarLinkArea").append('<a href="http://judah.cedarville.edu/~' + key + '/cs4220.html" class="crossbar">' + value + '</a>');
+				}
 			}
           });
         });
@@ -85,44 +89,138 @@
     }
     
     function getProjects(){
-	  var projectNames;
 	  var projects = [];
 	  $.post("ajax/getprojectnames.php",
 	  function(data){
-		updateGraphs(data);
+		getGraphData(data);
 	  });
     }
 	
 	
+	var t=setInterval(liveUpdate,5000);
 	
-	function updateGraphs(data){
-		projectNames = $.parseJSON(data)
+	function liveUpdate(){
+		if(isAProjectOpen){
+			isAProjectOpen = false;
+			getProjects();
+		}
+	}
+	
+	var isAProjectOpen = false;
+	var projectNames;
+	var graphsData = [];
+	var teamColors = {};
+	function getGraphData(data){
+		projectNames = $.parseJSON(data);
 		
 		$.each(projectNames, function(project, isOpen){
+			if(isOpen == 1){
+				isAProjectOpen = true;
+			}
 			$.get(
 			  "ajax/getpointsforproject.php",
 			  {"projectname":project},
 			  function(data){
-				console.log(JSON.stringify(data));
-				$.each(data, function(teamid, votedata){
-					
+				var scores = [];
+				var jsonObj = JSON.parse(data);
+				$.each(jsonObj, function(teamid, votedata){
+					if(!$.isEmptyObject(teamid)){
+						var myColor;
+						if(!(teamid in teamColors)){
+							myColor = randomColor({luminosity: 'dark'});
+							teamColors[teamid] = myColor;
+						}
+						else{
+							myColor = teamColors[teamid];
+						}
+						var fV = parseInt(votedata['firstVotes']);
+						var sV = parseInt(votedata['secondVotes']);
+						var tV = parseInt(votedata['thirdVotes']);
+						var score = fV * 3 + sV * 2 + tV;
+						var names = votedata['names'];
+						var nameStr = "";
+						var firstName = true;
+						$.each(names, function(index, name){
+							if(firstName){
+								nameStr = nameStr.concat(name);
+								firstName = false;
+							}
+							else {
+								nameStr = nameStr.concat(" & " + name);
+							}
+						});
+						
+						scores.push({
+							value: score,
+							color: myColor,
+							highlight: ColorLuminance(myColor, .1),
+							label: nameStr
+						});
+						
+					}
 				});
-			  }
-			);
+				updateGraphs(project, scores);
+			});
 		});
 	}
 	
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+	var graphs = {};
+	function updateGraphs(project, curGraphData){
+		curGraphData.sort(function(a, b){return b.value-a.value});
+		if(curGraphData.length >= 1){
+			curGraphData[0]['color'] = "#f7c950";
+			curGraphData[0]['highlight'] = ColorLuminance("#f7c950", .1);
+		}
+		if(curGraphData.length >= 2){
+			curGraphData[1]['color'] = "#c5c5c5";
+			curGraphData[1]['highlight'] = ColorLuminance("#c5c5c5", .1);
+		}
+		if(curGraphData.length >= 3){
+			curGraphData[2]['color'] = "#e8995f";
+			curGraphData[2]['highlight'] = ColorLuminance("#e8995f", .1);
+		}
+		
+		var canvasID = project.replace(/\s/g, '') + "Canvas";
+		var ctx = $("#" + canvasID).get(0).getContext("2d");
+		
+		if(curGraphData.length == 0){
+			ctx.font = "30px Arial";
+			ctx.fillStyle = "#202E4A";
+			ctx.fillText("No Votes on this Project",10,50);
+		}
+		else{
+			if(isAProjectOpen){
+				if(typeof graphs[project] != 'undefined'){
+					graphs[project].destroy();
+				}
+				graphs[project] = new Chart(ctx).Pie(curGraphData, {responsive : false, maintainAspectRatio: false, animateRotate: false});
+			}
+			else{
+				graphs[project] = new Chart(ctx).Pie(curGraphData, {responsive : false, maintainAspectRatio: false, animateRotate: true});
+			}
+			
+		}
+	}
+	
+	function ColorLuminance(hex, lum) {
+
+		// validate hex string
+		hex = String(hex).replace(/[^0-9a-f]/gi, '');
+		if (hex.length < 6) {
+			hex = hex[0]+hex[0]+hex[1]+hex[1]+hex[2]+hex[2];
+		}
+		lum = lum || 0;
+
+		// convert to decimal and change luminosity
+		var rgb = "#", c, i;
+		for (i = 0; i < 3; i++) {
+			c = parseInt(hex.substr(i*2,2), 16);
+			c = Math.round(Math.min(Math.max(0, c + (c * lum)), 255)).toString(16);
+			rgb += ("00"+c).substr(c.length);
+		}
+
+		return rgb;
+	}
     
     
 	
@@ -171,7 +269,6 @@
     <section id="projects">
 		<div id="tabs" class="container">
 		<ul  class="nav nav-tabs">
-			<li class="active"><a  href="#1b" data-toggle="tab">Overview</a></li>
 		<?php
 			include 'sqlserverparams.php';
 
@@ -192,30 +289,49 @@
 			}
 
 			$numProjects = mysqli_num_rows($result);
-			$curProj = 1;
 			if ($numProjects == 0){
 				echo "No data returned";
 			}else{
 				// our query returned at least one result. loop over results and do stuff.
+				$firstTab = True;
 				while($row = mysqli_fetch_assoc($result)){
-					echo "<li><a href=\"#p" . $curProj . "\" data-toggle=\"tab\">" . $row['name'] . "</a></li>";
-					$curProj++;
+					$projectID = $row['name'];
+					$projectID = str_replace(' ', '', $projectID);
+					if($firstTab){
+						echo "<li class=\"active\"><a href=\"#"  . $projectID .  "\" data-toggle=\"tab\">" . $row['name'] . "</a></li>";
+						$firstTab = false;
+					}
+					else {
+						echo "<li><a href=\"#"  . $projectID .  "\" data-toggle=\"tab\">" . $row['name'] . "</a></li>";
+					}
 				}
+				$name = $_SESSION[$nameParam];
+				if($name == "admin"){
+					echo "<li><a href=\"admin.php\">Admin</a></li>";
+				}
+				
+				
+				echo "</ul>";
+				echo "<div class=\"tab-content clearfix\">";
+				$result = $conn->query("SELECT name FROM pca.jk_projects;");
+				$firstTab = True;
+				while($row = mysqli_fetch_assoc($result)){
+					$projectName = $row['name'];
+					$projectID = $row['name'];
+					$projectID = str_replace(' ', '', $projectID);
+					if($firstTab){
+						echo "<div class=\"tab-pane active\" id=\"" . $projectID . "\"><h1>" . $projectName . "</h1>";
+						$firstTab = False;
+					}
+					else {
+						echo "<div class=\"tab-pane\" id=\"" . $projectID . "\"><h1>" . $projectName . "</h1>";
+					}
+					echo "<canvas id=\"" . $projectID ."Canvas\" height=\"500px\" width=\"500\">";
+					echo "</div>";
+				}
+				echo "</div>";				
 			}
 		?>
-		</ul>
-			<div class="tab-content clearfix">
-				<div class="tab-pane active" id="1b">
-					<h3>This is the default tab</h3>
-
-				</div>
-				<div class="tab-pane" id="p1">
-					<h3>This is project 1</h3>
-				</div>
-				<div class="tab-pane" id="p2">
-					<h3>This is project 2</h3>
-				</div>
-			</div>
 		</div>
     </section>
   </body>
